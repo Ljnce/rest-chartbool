07687e97702362a1c5b8c7e8360f26a5a4c864db
Luca <mylince@gmail.com>
2020-04-07 17:20:46 +0200

    Aggiunta grafico 2 senza %

--- js/main.js
+++ js/main.js
@@ -32,57 +32,101 @@ $.ajax({
                 'novembre': 0,
                 'dicembre': 0
             };
+            var venditeVenditore = {  } //Valore vuoto per i venditori
+
             var dati = data; // ho i miei dati
             for (var i = 0; i < dati.length; i++) {
                 var dato = dati[i] //estrapolo i singoli dati
-                // console.log(dato.id);
-                // console.log(dato.salesman);
+                // console.log(dato.id); //Non mi serve in questo caso
+                // console.log(dato.salesman); //Non mi serve in questo caso
                 // console.log(dato.amount); //Mi interessa l'ammontare delle somme
                 // console.log(dato.date); //Mi interessa la data, e troverò il mese
-                var mese = dato.date; // Mi creo la variabile che estrapolo la data
+                var venditore = dato.salesman;
+                var mese = dato.date; // Mi creo la variabile in cui estrapolo la data
                 var thisMonth = moment(mese, 'DD/MM/YYYY').format("MMMM"); //Trovo il mese che mi servirà per la somma
                 //console.log(thisMonth);
-                if (meseSomma[thisMonth] === undefined) { //Il colore lo do al mio valore vuoto
-                    meseSomma[thisMonth] = 0; //Se non essite, cioè se è uguale a 0 gli do 0
+                if (meseSomma[thisMonth] === undefined) { // MESI E VENDITE
+                    meseSomma[thisMonth] = 0;
                 }
                 meseSomma[thisMonth] += dato.amount;
-                //console.log(meseSomma[thisMonth]) ---> Mi trova tutti le somme dei signoli soggetti
+                if (venditeVenditore[venditore] === undefined) { // VENDITORI E VENDITE
+                    venditeVenditore[venditore] = 0;
+                }
+                venditeVenditore[venditore] += dato.amount;
+                //console.log(meseSomma[thisMonth]) ---> Mi trova tutti i valori dei signoli soggetti
             }
-                valoriFinali(meseSomma)
+                valoriFinali(meseSomma) //mesi e vendtiori
+                valoriFinaliVenditori(venditeVenditore) //venditori e fatturato
         },
         error: function(){
             alert('errore')
         }
     });
 
+//---------------> MESI E VENDITE <-----------------
+
+
 //Funzione per trovare  i valori finali
 function valoriFinali(meseSomma){
-    var labelsChart = [];
-    var dataChart = [];
+    var labelsChart = []; // Creazione variabile vuota per inserirci le mie date da richiamare poi nella chart
+    var dataChart = []; // Creazione variabile vuota per inserirci i miei valori da richiamare poi nella chart
 
     for (var key in meseSomma) {
         labelsChart.push(key);
         dataChart.push(meseSomma[key])
     }
     laMiaSomma(labelsChart, dataChart)
-}
+};
 
-//Assegno i valori finali trovati, alla mia CHART
+
+//Assegno i valori finali trovati, alla mia CHART per i valori mensili
 function laMiaSomma(labels, data){
     var ctx = $('#grafico');
     var chart = new Chart(ctx, {
-        // The type of chart we want to create
+
         type: 'line',
 
-        // The data for our dataset
         data: {
-            labels: labels,
+            labels: labels, //riporto il mio valore del mese
             datasets: [{
                 label: 'Database incassi annuali 2018',
                 backgroundColor: 'orange',
                 borderColor: 'orange',
-                data: data
+                data: data //riporto il mio valore dei valori
             }]
         },
     });
+};
+
+
+//---------------> VENDITORI E VENDITE <-----------------
+
+
+//Funzione per trovare i valori dei signoli venditori:
+function valoriFinaliVenditori(venditeVenditore){
+    var labelsVenditoreChart = [];
+    var dataVenditoreChart = [];
+
+    for (var key in venditeVenditore) {
+        labelsVenditoreChart.push(key);
+        dataVenditoreChart.push(venditeVenditore[key]);
+    }
+    laMiaSommaVenditori(labelsVenditoreChart, dataVenditoreChart)
+};
+
+//Assegno i valori finali trovati, alla mia CHART per i valori dei venditori
+function laMiaSommaVenditori(labels2, data2){
+    var ctx = $('#grafico-torta');
+    var chart = new Chart(ctx, {
+
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: data2,
+                backgroundColor: ['red', 'green', 'blue', 'yellow']
+            }],
+
+            labels: labels2
+        }
+    })
 };
