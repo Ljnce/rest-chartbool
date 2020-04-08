5e04d47719cae247941d5050a8e2449e8316ed2b
Luca <mylince@gmail.com>
2020-04-08 10:50:54 +0200

    In progress part 2

--- js/main.js
+++ js/main.js
@@ -13,55 +13,63 @@
 // Novembre:  tot amount
 // Dicembre:  tot amount
 
-$.ajax({
+//Le mie 2 variabili vuote che poi andrò a riempire
+var meseSomma = { //Prendo i valori in ordine e ci aggiungo i risultati corrispondenti
+    'gennaio': 0,
+    'febbraio': 0,
+    'marzo': 0,
+    'aprile': 0,
+    'maggio': 0,
+    'giugno': 0,
+    'luglio': 0,
+    'agosto': 0,
+    'settembre': 0,
+    'ottobre': 0,
+    'novembre': 0,
+    'dicembre': 0
+};
 
+var venditeVenditore = {} //Valore vuoto per i venditori
+
+//Ajax
+$.ajax({
         url: 'http://157.230.17.132:4014/sales',
         method: 'GET',
         success: function(data){
-            var meseSomma = { //Prendo i valori in ordine e ci aggiungo i risultati corrispondenti
-                'gennaio': 0,
-                'febbraio': 0,
-                'marzo': 0,
-                'aprile': 0,
-                'maggio': 0,
-                'giugno': 0,
-                'luglio': 0,
-                'agosto': 0,
-                'settembre': 0,
-                'ottobre': 0,
-                'novembre': 0,
-                'dicembre': 0
-            };
-            var venditeVenditore = {  } //Valore vuoto per i venditori
-
-            var dati = data; // ho i miei dati
-            for (var i = 0; i < dati.length; i++) {
-                var dato = dati[i] //estrapolo i singoli dati
-                // console.log(dato.id); //Non mi serve in questo caso
-                // console.log(dato.salesman); //Non mi serve in questo caso
-                // console.log(dato.amount); //Mi interessa l'ammontare delle somme
-                // console.log(dato.date); //Mi interessa la data, e troverò il mese
-                var venditore = dato.salesman;
-                var mese = dato.date; // Mi creo la variabile in cui estrapolo la data
-                var thisMonth = moment(mese, 'DD/MM/YYYY').format("MMMM"); //Trovo il mese che mi servirà per la somma
-                //console.log(thisMonth);
-                if (meseSomma[thisMonth] === undefined) { // MESI E VENDITE
-                    meseSomma[thisMonth] = 0;
-                }
-                meseSomma[thisMonth] += dato.amount;
-                if (venditeVenditore[venditore] === undefined) { // VENDITORI E VENDITE
-                    venditeVenditore[venditore] = 0;
-                }
-                venditeVenditore[venditore] += dato.amount;
-                //console.log(meseSomma[thisMonth]) ---> Mi trova tutti i valori dei signoli soggetti
-            }
-                valoriFinali(meseSomma) //mesi e vendtiori
-                valoriFinaliVenditori(venditeVenditore) //venditori e fatturato
+            var costruttore = costruttoreData(data);
         },
         error: function(){
             alert('errore')
         }
     });
+
+//Funzione per contenuto ajax
+function costruttoreData(data){
+    var dati = data; // ho i miei dati
+    for (var i = 0; i < dati.length; i++) {
+        var dato = dati[i] //estrapolo i singoli dati
+        // console.log(dato.id); //Non mi serve in questo caso
+        // console.log(dato.salesman); //Non mi serve in questo caso
+        // console.log(dato.amount); //Mi interessa l'ammontare delle somme
+        // console.log(dato.date); //Mi interessa la data, e troverò il mese
+        var venditore = dato.salesman;
+        var mese = dato.date; // Mi creo la variabile in cui estrapolo la data
+        var thisMonth = moment(mese, 'DD/MM/YYYY').format("MMMM"); //Trovo il mese che mi servirà per la somma
+        //console.log(thisMonth);
+        if (meseSomma[thisMonth] === undefined) { // MESI E VENDITE
+            meseSomma[thisMonth] = 0;
+        }
+        meseSomma[thisMonth] += dato.amount;
+        if (venditeVenditore[venditore] === undefined) { // VENDITORI E VENDITE
+            venditeVenditore[venditore] = 0;
+        }
+        venditeVenditore[venditore] += dato.amount;
+        //console.log(meseSomma[thisMonth]) ---> Mi trova tutti i valori dei signoli soggetti
+    }
+        valoriFinali(meseSomma) //mesi e vendtiori
+        valoriFinaliVenditori(venditeVenditore) //venditori e fatturato
+};
+
 
 //---------------> MESI E VENDITE <-----------------
 
@@ -73,9 +81,9 @@ function valoriFinali(meseSomma){
 
     for (var key in meseSomma) {
         labelsChart.push(key);
-        dataChart.push(meseSomma[key])
+        dataChart.push(meseSomma[key]);
     }
-    laMiaSomma(labelsChart, dataChart)
+    laMiaSomma(labelsChart, dataChart);
 };
 
 
@@ -111,7 +119,7 @@ function valoriFinaliVenditori(venditeVenditore){
         labelsVenditoreChart.push(key);
         dataVenditoreChart.push(Math.round(venditeVenditore[key] * 10 / 118.940)); //Circa un %
     }
-    laMiaSommaVenditori(labelsVenditoreChart, dataVenditoreChart)
+    laMiaSommaVenditori(labelsVenditoreChart, dataVenditoreChart);
 };
 
 //Assegno i valori finali trovati, alla mia CHART per i valori dei venditori
